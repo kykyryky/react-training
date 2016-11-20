9079178e2c2eaa427afc4af6a8d6f2c83b745b95
azharov <alexander.zharov.spb@gmail.com>
2016-11-20 16:20:24 +0300

    develop_1

---
+++ src/header/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+class Header extends Component {
+    render() {
+        return (
+            <div className='header'>
+                <h1>
+                    To-Do List
+                </h1>
+            </div>
+        );   
+    }
+}
+
+export default Header;
