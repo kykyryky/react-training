381859fbe4ba69977b6cd017d70fabaa7352ef79
Aleksandr Zharov <alexander.zharov.spb@gmail.com>
2017-01-14 13:54:18 +0300

    fixed styles a bit

--- src/common/SearchBar.js
+++ src/common/SearchBar.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import { browserHistory } from 'react-router';
+import './SearchBar.css';
 
 class SearchBar extends Component {
     constructor(props) {
@@ -17,23 +18,25 @@ class SearchBar extends Component {
 
     render() {
         return (
-            <div className='constainer'>
-                <div className='offset-md-9'>
-                    <div className="form-group form-inline">
-                        <input type="text"
-                            value={this.props.filterTextInput}
-                            onChange={this.handleChange.bind(this)} 
-                            ref={(input) => this.filterTextInput = input}
-                            className="form-control col-offset-md-2" 
-                            placeholder="Search..."/>
+            <div className='search-bar'>
+                <div className='constainer'>
+                    <div className='offset-md-9'>
+                        <div className="form-group form-inline">
+                            <input type="text"
+                                value={this.props.filterTextInput}
+                                onChange={this.handleChange.bind(this)} 
+                                ref={(input) => this.filterTextInput = input}
+                                className="form-control col-offset-md-2 mr-20" 
+                                placeholder="Search..."/>
 
-                        <label>
-                            <input
-                                type="checkbox"
-                                checked={this.props.onlyDone}
-                                ref={(input) => this.onlyDone = input}
-                                onChange={this.handleChange.bind(this)}/> Done
-                        </label>
+                            <label>
+                                <input
+                                    type="checkbox"
+                                    checked={this.props.onlyDone}
+                                    ref={(input) => this.onlyDone = input}
+                                    onChange={this.handleChange.bind(this)}/> Done
+                            </label>
+                        </div>
                     </div>
                 </div>
             </div>
